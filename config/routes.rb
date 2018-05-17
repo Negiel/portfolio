43dfbc0a00f9864712b36170ee98598b853add80
Negiel <negiel1@gmail.com>
2018-05-17 15:17:55 -0500

    all pages and links done

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'portfolio' => 'pages#portfolio', as: :portfolio
   get 'welcome' => 'pages#welcome', as: :welcome
   get 'about' => 'pages#about', as: :about
+  get 'contact' => 'pages#contact', as: :contact
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
